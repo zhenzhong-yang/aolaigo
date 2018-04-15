5ee5e219c8c019b813fc92f529736ffda332cbfa
yangzhenzhong <1564594495@qq.com>
2018-04-15 18:19:23 +0800

    完善注册功能

--- src/js/register.js
+++ src/js/register.js
@@ -16,6 +16,40 @@ require(['config'],function(){
             let password1 = $('#password1').val();
             let password2 = $('#password2').val();
 
+            if(phone === ""){
+                alert("手机号不能为空");
+                $('#phone').focus();
+                 return false;
+            }else if(!/^[1][3,4,5,7,8][0-9]{9}$/.test(phone)){
+                alert("手机号不合法");
+                $('#phone').focus();
+                return false;
+            }else if(password1 === ""){
+                alert("密码不能为空");
+                $('#password1').focus();
+                return false;
+            }else if(password2 === ""){
+                alert("确认密码不能为空");
+                $('#password2').focus();
+                return false;
+            }else if(password1 != password2){
+                alert("两次密码不同,请重新输入");
+                $('#password2').focus();
+                return false;
+            }else if(!/^[\d\D]{6,12}$/.test(password1)){
+                alert("密码格式不正确");
+                $('#password1').focus();
+                return false;
+            }else if($('#text').val() == ''){
+                alert("请输入验证码");
+                $('#text').focus();
+                return false;
+            }else if($('#text').val() != code.html()){
+                alert("验证码错误");
+                $('#text').focus();
+                return false;
+            }
+
             $.ajax({
                 type:'get',
                 data:{'phone':phone,'password':password1,'type':type},
@@ -23,30 +57,11 @@ require(['config'],function(){
                 dataType:'html',
                 success:function(res){
                     console.log(res);
-
-                    if(phone === ""){
-                        alert("手机号不能为空");
-                    }else if(!/^[1][3,4,5,7,8][0-9]{9}$/.test(phone)){
-                        alert("手机号不合法");
-                        return false;
-                    }else if(password1 === ""){
-                        alert("密码不能为空");
-                    }else if(password2 === ""){
-                        alert("确认密码不能为空");
-                    }else if(password1 != password2){
-                        alert("两次密码不同,请重新输入");
-                    }else if(!/^[\d\D]{6,12}$/.test(password1)){
-                        alert("密码不正确");
-                        return false;
-                    }else if(res == "no"){
+                    if(res == "no"){
                         alert("该用户已存在");
-                    }else if($('#text').val() == ''){
-                        alert("请输入验证码");
-                    }else if($('#text').val() != code.html()){
-                        alert("验证码错误");
-                    }else{
-                       location.href = '../index.html';
                     }
+                    location.href = '../html/login.html';
+                    
                 }
             });
         });
