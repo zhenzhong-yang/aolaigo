80796974e2d123c901e649503804442a34ae89d6
yangzhenzhong <1564594495@qq.com>
2018-04-15 18:12:39 +0800

    添加新的商品，并完善升序降序和分页功能

--- src/api/goodslist.php
+++ src/api/goodslist.php
@@ -1,11 +1,13 @@
 <?php
+
     $conn = new mysqli("localhost","root","root","aolaigo");
     $conn->set_charset("utf8");
 
     $page = isset($_GET['page']) ? $_GET['page'] : 1;
     $qty = $_GET['qty'];
+    $goods_class = $_GET['goods_class'];
 
-    $sql = "select * from goods";
+    $sql = "select * from goods where goods.goods_class = '$goods_class'";
     $data = $conn->query($sql)->fetch_all(MYSQL_ASSOC);
 
     if($qty == ''){
@@ -16,7 +18,8 @@
             "total" => count($data),
             "data" => array_slice($data,$qty*($page-1),$qty),
             "qty" => $qty*1,
-            "page" => $page*1
+            "page" => $page*1,
+            "goods_class" =>$goods_class
         );
 
         echo json_encode($res,JSON_UNESCAPED_UNICODE);
--- src/js/goodslist.js
+++ src/js/goodslist.js
@@ -3,6 +3,7 @@
 // });
 require(['config'],function(){
     require(['jQuery','jquerySession','caidan'],function(){
+
         $('header').load('../html/head.html',function(){
 
             /*获取当前用户名*/
@@ -18,13 +19,25 @@ require(['config'],function(){
             caidan();
         });
 
+        let goods_class;
+          //获取url中的参数
+        function getUrlParam(name) {
+           var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+            console.log(reg);
+           var r = window.location.search.substr(1).match(reg); //匹配目标参数
+           if (r != null) return decodeURIComponent(r[2]); return ''; //返回参数值
+        }
+          //接收URL中的参数
+        goods_class = getUrlParam('goods_class');
+        console.log(goods_class);
+
         let qty = 18;
 
         /*发起请求*/
         function sendAjax(){
             $.ajax({
                 type:'get',
-                data:{'qty':qty},
+                data:{'qty':qty,'goods_class':goods_class},
                 url:'../api/goodslist.php',
                 dataType:'json',
                 success:function(res){
@@ -40,11 +53,14 @@ require(['config'],function(){
         function add(res){
             $('#goodslist').html(res.data.map(function(item){
                 console.log(item);
-                return `<li>
-                    <a href='details.html?goodsid=${item.goods_id}'><img src="${item.goods_imgurl}"/></a>
-                    <p><span>￥${item.goods_price}</span><del>￥${item.goods_yuanjia}</del></p>
-                    <p>${item.goods_name}</p>
-                </li>`
+                // if(item.goods_class == goods_class){
+                    return `<li>
+                        <a href='details.html?goodsid=${item.goods_id}&goods_class=${goods_class}'><img src="${item.goods_imgurl}" style="width:100%;height:100%;"/></a>
+                        <p><span>￥${item.goods_price}</span><del>￥${item.goods_yuanjia}</del></p>
+                        <p>${item.goods_name}</p>
+                    </li>`
+                // }
+
             }));
 
             /*分页*/
@@ -71,7 +87,7 @@ require(['config'],function(){
                 let pageNo = $(this).html();
                 $.ajax({
                     type:'get',
-                    data:{'qty':qty,'page':pageNo},
+                    data:{'qty':qty,'page':pageNo,'goods_class':goods_class},
                     url:'../api/goodslist.php',
                     dataType:'json',
                     success:function(res){
@@ -82,9 +98,7 @@ require(['config'],function(){
             });
         }
 
-
-
-
+        /*综合排序*/
         $('#zong').on('click',function(){
             sendAjax();
             active();
@@ -93,8 +107,6 @@ require(['config'],function(){
 
         let num = 17;
 
-
-        
         $('dl').on('click','dd',function(){
             console.log(666);
             $(this).addClass('dd');
@@ -105,13 +117,15 @@ require(['config'],function(){
         let dianji = 1;
         $('#shipai').on('click',function(){
             console.log($(this));
+            /*当前按钮*/
             let dis = $(this);
             dis.find('i').show();
             let pai = 'pai';
+            /*时间升序*/
             if(dianji == 1){
                 $.ajax({
                     type:'get',
-                    data:{'num':num,'pai':pai},
+                    data:{'num':num,'pai':pai,'goods_class':goods_class},
                     url:'../api/time.php',
                     dataType:'json',
                     success:function(res){console.log(666);
@@ -122,10 +136,11 @@ require(['config'],function(){
                 $('#page').hide();
                 $('#shipai').find('img').attr('src','../images/jiang.png');
                 dianji = 2;
+            /*时间降序*/
             }else{
                 $.ajax({
                     type:'get',
-                    data:{'num':num,'pai':''},
+                    data:{'num':num,'pai':'','goods_class':goods_class},
                     url:'../api/time.php',
                     dataType:'json',
                     success:function(res){console.log(666);
@@ -137,6 +152,7 @@ require(['config'],function(){
                 $('#shipai').find('img').attr('src','../images/sheng.png');
                 dianji = 1;
             }
+            /*当这个按钮的其他兄弟元素点击时图片隐藏*/
             dis.siblings('dd').on('click',function(){
                 console.log(123456);
                 dis.find('i').hide();
@@ -149,10 +165,11 @@ require(['config'],function(){
             let dis = $(this);
             dis.find('i').show();
             let pai = 'pai';
+            /*价格降序*/
             if(dianji == 1){
                 $.ajax({
                     type:'get',
-                    data:{'num':num,'pai':''},
+                    data:{'num':num,'pai':'','goods_class':goods_class},
                     url:'../api/price.php',
                     dataType:'json',
                     success:function(res){console.log(666);
@@ -163,10 +180,11 @@ require(['config'],function(){
                 $('#page').hide();
                 $('#jiapai').find('img').attr('src','../images/sheng.png');
                 dianji = 2;
+            /*价格升序*/
             }else{
                 $.ajax({
                     type:'get',
-                    data:{'num':num,'pai':pai},
+                    data:{'num':num,'pai':pai,'goods_class':goods_class},
                     url:'../api/price.php',
                     dataType:'json',
                     success:function(res){console.log(666);
@@ -183,34 +201,80 @@ require(['config'],function(){
                 dis.find('i').hide();
             });
         });
-
+    /*排序后写入写入页面的函数*/
     function hasPai(res){
         $('#goodslist').html(res.map(function(item){
             console.log(item);
             return `<li>
-                <a href='details.html?goodsid=${item.goods_id}'><img src="${item.goods_imgurl}"/></a>
+                <a href='details.html?goodsid=${item.goods_id}&goods_class=${goods_class}'><img src="${item.goods_imgurl}"/></a>
                 <p><span>￥${item.goods_price}</span><del>￥${item.goods_yuanjia}</del></p>
                 <p>${item.goods_name}</p>
             </li>`
         }));
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        $('footer').load('../html/footer.html');
+        /*从数据库获取图片写入首页*/
+        $.ajax({
+            type:'get',
+            url:'../api/index.php',
+            dataType:'json',
+            success:function(res){
+                // console.log(res);
+                $('.goodsUl1').html(res.map(function(item){
+                    // console.log(item.img_url);
+                    if(item.img_name == 'z'){
+                        return `<li class="li">
+                        <a href=""><img src="../${item.img_url}"></a>
+                        </li>`
+                    }
+                }));
+                $('.goodsUl2').html(res.map(function(item){
+                    // console.log(item.img_url);
+                    if(item.img_name == 's'){
+                        return `<li class="li">
+                        <a href=""><img src="../${item.img_url} " height="64" width="112" ></a>
+                        </li>`
+                    }
+                }));
+                $('.goodsUl3').html(res.map(function(item){
+                    // console.log(item.img_url);
+                    if(item.img_name == 'k'){
+                        return `<li class="li">
+                        <a href=""><img src="../${item.img_url}" height="64" width="112" ></a>
+                        </li>`
+                    }
+                }));
+                $('.goodsUl4').html(res.map(function(item){
+                    // console.log(item.img_url);
+                    if(item.img_name == 'y'){
+                        return `<li class="li">
+                        <a href=""><img src="../${item.img_url}"></a>
+                        </li>`
+                    }
+                }));
+            }
+        });
         
+
+
+        $('footer').load('../html/footer.html',function(){
+            /*获取当前用户名商品数量*/
+            let goodsNum = $.session.get('goodsNumber');
+            console.log(goodsNum);
+            $('.goodsNum').html(goodsNum);
+            /*点击返回顶部*/
+            $('#goTop').on('click',function(){
+                var timer = setInterval(function(){
+                    scroll = window.scrollY;
+                    var speed = parseInt(scroll/5)
+                    if(scroll <= 10){
+                        clearInterval(timer);
+                        /*设置浏览器滚动的距离*/
+                        window.scrollTo(0,0);
+                    }
+                    scrollBy(0,-speed);
+                },50);
+            });
+        });
     });
 });
